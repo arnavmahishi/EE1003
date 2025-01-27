cf1fb955a14efe0b6108abbac82ca3476f662edd
arnavmahishi <arnavmahishi123@gmail.com>
2025-01-27 12:46:40 +0530

    add files

--- assignments/Problem 8/codes/lottery.c
+++ assignments/Problem 8/codes/lottery.c
@@ -5,25 +5,58 @@
 #define TOTAL_NUMBERS 20 // Total numbers in the lottery
 #define CHOICES 6        // Numbers chosen by the player
 
+// Function to generate a unique set of numbers
+void generate_unique_numbers(int* numbers, int num_choices) {
+    int i, j;
+    for (i = 0; i < num_choices; i++) {
+        numbers[i] = 1 + (int)(rand() % TOTAL_NUMBERS);
+        // Ensure the generated number is unique
+        for (j = 0; j < i; j++) {
+            if (numbers[i] == numbers[j]) {
+                i--; // Regenerate this number
+                break;
+            }
+        }
+    }
+}
+
 // Function to simulate the lottery and track relative frequency
 void simulate_lottery(int simulations, int interval, double* results) {
     int win_count = 0;
 
     // Seed the random number generator
-    srand(time(0));
+    srand(time(NULL));
 
     for (int i = 0; i < simulations; i++) {
         int fixed_numbers[CHOICES], chosen_numbers[CHOICES];
         int is_match = 1;
 
         // Generate fixed lottery numbers
-        for (int j = 0; j < CHOICES; j++) {
-            fixed_numbers[j] = (rand() % TOTAL_NUMBERS) + 1;
+        generate_unique_numbers(fixed_numbers, CHOICES);
+
+        // Sort the fixed numbers
+        for (int j = 0; j < CHOICES - 1; j++) {
+            for (int k = j + 1; k < CHOICES; k++) {
+                if (fixed_numbers[j] > fixed_numbers[k]) {
+                    int temp = fixed_numbers[j];
+                    fixed_numbers[j] = fixed_numbers[k];
+                    fixed_numbers[k] = temp;
+                }
+            }
         }
 
         // Generate player's chosen numbers
-        for (int j = 0; j < CHOICES; j++) {
-            chosen_numbers[j] = (rand() % TOTAL_NUMBERS) + 1;
+        generate_unique_numbers(chosen_numbers, CHOICES);
+
+        // Sort the chosen numbers
+        for (int j = 0; j < CHOICES - 1; j++) {
+            for (int k = j + 1; k < CHOICES; k++) {
+                if (chosen_numbers[j] > chosen_numbers[k]) {
+                    int temp = chosen_numbers[j];
+                    chosen_numbers[j] = chosen_numbers[k];
+                    chosen_numbers[k] = temp;
+                }
+            }
         }
 
         // Check for match
@@ -44,3 +77,21 @@ void simulate_lottery(int simulations, int interval, double* results) {
         }
     }
 }
+
+int main() {
+    int simulations = 100000; // Example number of simulations
+    int interval = 1000;      // Interval for frequency tracking
+    int result_size = simulations / interval;
+    double* results = (double*)malloc(result_size * sizeof(double));
+
+    simulate_lottery(simulations, interval, results);
+
+    // Output the results
+    for (int i = 0; i < result_size; i++) {
+        printf("Interval %d: Relative Frequency = %.6f\n", i + 1, results[i]);
+    }
+
+    free(results);
+    return 0;
+}
+
--- assignments/Problem 8/codes/lottery.py
+++ assignments/Problem 8/codes/lottery.py
@@ -9,8 +9,8 @@ lottery = ctypes.CDLL('./lottery.so')
 lottery.simulate_lottery.argtypes = [ctypes.c_int, ctypes.c_int, ctypes.POINTER(ctypes.c_double)]
 
 # Parameters
-simulations = 1000000  # Total number of simulations
-interval = 10000       # Interval for tracking relative frequency
+simulations = 10000000  # Total number of simulations
+interval = 10000        # Interval for tracking relative frequency
 num_intervals = simulations // interval
 
 # Allocate space for results
@@ -21,7 +21,7 @@ lottery.simulate_lottery(simulations, interval, relative_frequencies)
 
 # Convert results to NumPy array
 relative_frequencies = np.array(relative_frequencies)
-
+print(relative_frequencies)
 # Compute the true probability
 total_combinations = np.math.comb(20, 6)
 true_probability = 1 / total_combinations
@@ -55,11 +55,22 @@ plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.savefig('../figs/cdf.png')
 plt.show()
 
-# Plot convergence of relative frequency
+# Plot convergence of relative frequency with zoomed-in y-axis in terms of 1e-5
 plt.figure(figsize=(10, 6))
 x_values = range(interval, simulations + 1, interval)
 plt.plot(x_values, relative_frequencies, label='Relative Frequency', color='blue')
 plt.axhline(true_probability, color='red', linestyle='--', label='True Probability')
+
+# Zoom in on the y-axis with terms of 1e-5
+# Scale the y-axis values to be in the range of 1e-5
+y_min = np.floor(min(min(relative_frequencies), true_probability) * 1e5) / 1e5
+y_max = np.ceil(max(max(relative_frequencies), true_probability) * 1e5) / 1e5
+
+plt.ylim(y_min, y_max)
+
+# Set ticks for y-axis to be in terms of 1e-5 intervals
+plt.yticks(np.arange(y_min, y_max + 0.00001, 0.00001))
+
 plt.xlabel('Number of Trials')
 plt.ylabel('Probability')
 plt.title('Convergence of Relative Frequency to True Probability')
@@ -67,3 +78,4 @@ plt.legend()
 plt.grid(alpha=0.7)
 plt.savefig('../figs/fig.png')
 plt.show()
+
